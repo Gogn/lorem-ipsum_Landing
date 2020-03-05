8f53c67171cd9d0d20ebbe9205f4964980f19893
Dero <106393@gmail.com>
2020-03-05 17:24:58 +0400

    Updates

---
+++ precache-manifest.470445b6147cd1e194585fd170564af4.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7611398fe24426cd976c0b5a9158dea1",
+    "url": "/lorem-ipsum_landing/index.html"
+  },
+  {
+    "revision": "16eb8f5c17acf247dd8a",
+    "url": "/lorem-ipsum_landing/static/css/2.59ca64ee.chunk.css"
+  },
+  {
+    "revision": "b03bda4d52cd8fb5c7ca",
+    "url": "/lorem-ipsum_landing/static/css/main.ef6c36dc.chunk.css"
+  },
+  {
+    "revision": "16eb8f5c17acf247dd8a",
+    "url": "/lorem-ipsum_landing/static/js/2.0d46bea9.chunk.js"
+  },
+  {
+    "revision": "ff470f15f20efad4419b730894456a5f",
+    "url": "/lorem-ipsum_landing/static/js/2.0d46bea9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b03bda4d52cd8fb5c7ca",
+    "url": "/lorem-ipsum_landing/static/js/main.ba7a1e4e.chunk.js"
+  },
+  {
+    "revision": "866f0fdc8d36cd10d89f",
+    "url": "/lorem-ipsum_landing/static/js/runtime-main.d9cbddad.js"
+  },
+  {
+    "revision": "dad575741a731c74876e68b2aa270986",
+    "url": "/lorem-ipsum_landing/static/media/Dot.dad57574.svg"
+  },
+  {
+    "revision": "0310003232b4b6424bffeac67e6fbed5",
+    "url": "/lorem-ipsum_landing/static/media/MuseoSansCyrl-100.03100032.ttf"
+  },
+  {
+    "revision": "9c67b05570bc2d24c03962e07f37470f",
+    "url": "/lorem-ipsum_landing/static/media/MuseoSansCyrl-100.9c67b055.woff"
+  },
+  {
+    "revision": "a61eab68beb7bd4101cf05a752c7ac71",
+    "url": "/lorem-ipsum_landing/static/media/MuseoSansCyrl-100.a61eab68.eot"
+  },
+  {
+    "revision": "3345ad51891c7dbc5868f1f10ae40e6b",
+    "url": "/lorem-ipsum_landing/static/media/Museosanscyrl500.3345ad51.woff2"
+  },
+  {
+    "revision": "d0c7ec833dd85c52fb59a59babe6e1ba",
+    "url": "/lorem-ipsum_landing/static/media/Museosanscyrl500.d0c7ec83.ttf"
+  },
+  {
+    "revision": "d97960c3800c88cb8ccab3527443ef97",
+    "url": "/lorem-ipsum_landing/static/media/Museosanscyrl500.d97960c3.woff"
+  },
+  {
+    "revision": "1e49b741ab3430b2adb941ae55a99ef0",
+    "url": "/lorem-ipsum_landing/static/media/bebasneueregular.1e49b741.ttf"
+  },
+  {
+    "revision": "3216eb8b241526bd0e409cf060a8fa03",
+    "url": "/lorem-ipsum_landing/static/media/bebasneueregular.3216eb8b.woff"
+  },
+  {
+    "revision": "a7c6c96bd21227e0ade223071d0ae01c",
+    "url": "/lorem-ipsum_landing/static/media/bebasneueregular.a7c6c96b.woff2"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/lorem-ipsum_landing/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/lorem-ipsum_landing/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/lorem-ipsum_landing/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/lorem-ipsum_landing/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lorem-ipsum_Landing/precache-manifest.131d5b4dca3785857935ab6e9c25e6f6.js"
+  "/lorem-ipsum_landing/precache-manifest.470445b6147cd1e194585fd170564af4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lorem-ipsum_Landing/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lorem-ipsum_landing/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
